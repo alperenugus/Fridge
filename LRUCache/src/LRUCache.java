ce8f766b11b6ed124cfaf49e9a468a93813985ed
alperenugus <alperenugus@gmail.com>
2021-08-16 23:45:02 -0700

    LRU cache will be implemented.

--- LRUCache/src/LRUCache.java
+++ LRUCache/src/LRUCache.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class LRUCache {
+import java.util.HashMap;
+
+public class LRUCache {
+
+    HashMap<Integer, Integer> cache;
+
+
+    public LRUCache(int capacity) {
+        cache = new HashMap<>(capacity);
+    }
+
+    public int get(int key) {
+
+        if(!cache.containsKey(key)) return -1;
+        return cache.get(key);
+    }
+
+    public void put(int key, int value) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
