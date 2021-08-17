02163993bf63c2439a0cdd1f3dbc8ae81db6396b
alperenugus <alperenugus@gmail.com>
2021-08-16 18:11:09 -0700

    Delete method for trie added.

--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -24,6 +24,35 @@ public class Trie {
         current.isWord = true;
     }
 
+    public void delete(String word) {
+        delete(root, word, 0);
+    }
+
+    public boolean delete(TrieNode current, String word, int index){
+
+        // Checks for the last character in the word
+        if(index == word.length()){
+            if(!current.isWord) return false;
+            current.isWord = false;
+            return current.children.isEmpty();
+        }
+
+        char c = word.charAt(index);
+
+        TrieNode node = current.children.get(c);
+        if(node == null) return false;
+
+        // For each word, check if it should be removed
+        boolean shouldDeleteCurrentNode = delete(node, word, index+1) && !node.isWord;
+
+        if(shouldDeleteCurrentNode){
+            current.children.remove(c);
+            return current.children.isEmpty();
+        }
+
+        return false;
+    }
+
     public boolean search(String word) {
 
         char[] arr = word.toCharArray();
@@ -66,6 +95,9 @@ public class Trie {
         System.out.println(trie.startsWith("app"));
 
         trie.insert("app");
+        System.out.println(trie.search("app"));
+
+        trie.delete("app");
         System.out.println(trie.search("app"));
 
     }
